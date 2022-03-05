f56b7cef034d80e081b541e10c4ace467bb3c392
yashaswini-my <100765176+yashaswini-my@users.noreply.github.com>
2022-03-05 23:36:48 +0530

    Update StepDefHotelBooking.java

--- src/test/java/step_definition/StepDefHotelBooking.java
+++ src/test/java/step_definition/StepDefHotelBooking.java
@@ -17,7 +17,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 //import pageBean.HotelBookingPageFactory;
-import io.cucumber.datatable.DataTable;
+//import io.cucumber.datatable.DataTable;
 
 public class StepDefHotelBooking {
 	private WebDriver driver;
